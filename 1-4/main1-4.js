6a5f907dfeffd4a5017fcb6679a562bff9eca8ba
daiki05040504 <68774247+daiki05040504@users.noreply.github.com>
2020-08-18 23:29:19 +0900

    課題提出
    お疲れ様です。
    課題提出します。

---
+++ 1-4/main1-4.js
@@ -0,0 +1,46 @@
+// 3の倍数のとき、 Fizz! 
+
+let number = 1;
+while( number <= 100; number++) {
+  console.log(number)
+}
+
+if (number % 3 === 0) {
+  console.log(Fezz!!);
+
+
+
+// 5の倍数のとき、 Buzz! と出力します。
+let number = 1;
+while( number <= 100; number++) {
+  console.log(number)
+}
+
+if (number % 5 === 0) {
+  console.log(Fezz!!);
+
+// 3の倍数かつ5の倍数のとき、 FizzBuzz!! と出力します。(15の倍数、と考えるのはナシです。)
+
+let number = 1;
+while( number <= 100; number++) {
+  console.log(number)
+}
+
+if (number % 5 === 0, number % 3 === 0) {
+  console.log(Fezz!!);
+
+
+// 3の倍数でも、5の倍数でもない場合は、その数を出力します。
+// それを1〜100まで実施してください。
+
+let number = 1;
+while( number <= 100; number++) {
+  console.log(number)
+}
+
+if (number % 5 === 0, number % 3 === 0) {
+  console.log(Fezz!!);
+
+  else{
+    console.log(number);
+  }
